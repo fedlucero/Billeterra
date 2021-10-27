8b17369fee896ab3ffe256d1d1e8f762c78cd925
Federico Lucero <cfede.lucero@gmail.com>
2021-10-26 23:09:48 -0300

    animaciones

---
+++ js/Billeterra.js
@@ -0,0 +1,18 @@
+// $("#navActivo").on("click", function(){
+//     $("#monedasCard").hide();
+// });
+
+
+$("#navActivo").on("click", function(){
+        $("#monedasCard")
+    .hide()
+    .delay(2000)
+    .show();
+    });
+    
+
+//  $("#navActivo").on("click", function(){
+//     $("#monedasCard")
+//     .slideUp(1000)
+//     .slideDown(1000);
+// });
--- js/inicio.js
+++ js/inicio.js
@@ -1,27 +1,40 @@
 
 
-$("#btnTerminos").on("click", function(){
-    $("#tyc").hide();
-});
+// $("#btnTerminos").on("click", function(){
+//     $("#tyc").hide();
+// });
+
+// $("#btnTerminos").on("click", function(){
+//     $("#btnTerminos").hide();
+// });
+
+// $("#btnTerminos").on("click", function(){
+//     $("#btnTerminos2").show();
+// });
+
+// $("#btnTerminos2").on("click", function(){
+//     $("#btnTerminos2").hide();
+// });
+
+// $("#btnTerminos2").on("click", function(){
+//     $("#tyc").show();
+// });
+
+// $("#btnTerminos2").on("click", function(){
+//     $("#btnTerminos").show();
+
+
+// })
 
 $("#btnTerminos").on("click", function(){
-    $("#btnTerminos").hide();
-});
-
-$("#btnTerminos").on("click", function(){
+    $("#tyc").hide()
+    $("#btnTerminos").hide()
     $("#btnTerminos2").show();
 });
 
 $("#btnTerminos2").on("click", function(){
     $("#btnTerminos2").hide();
-});
-
-$("#btnTerminos2").on("click", function(){
     $("#tyc").show();
-});
-
-$("#btnTerminos2").on("click", function(){
     $("#btnTerminos").show();
-
-
 })
+
