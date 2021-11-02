c64911108b649d114c6f4ffa5b571d79f2e821e9
Federico Lucero <cfede.lucero@gmail.com>
2021-11-02 16:06:55 -0300

    vuelta atras

--- js/Billeterra.js
+++ js/Billeterra.js
@@ -71,7 +71,7 @@ window.addEventListener('load', () => {
                                     <div class="apiDiv" id="${coin.id}">
                                     
                                         <div class="apiHijo">
-                                            <img src="../imagenes/(${coin.symbol}.toLowerCase()).png">
+                                            <img src="../imagenes/(${coin.symbol}.png">
                                         </div>
                                         <div class="apiHijo">
                                             ${coin.name}
